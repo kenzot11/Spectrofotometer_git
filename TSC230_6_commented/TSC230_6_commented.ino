be55e752d22b3bb54e6de1e8cc17aa94cdd71e79
kenzot11 <r0628842@student.thomasmore.be>
2017-02-16 14:34:50 +0100

    programma gecompileerd nog testen
    nog testen of dit aangepaste en verduidelijkte programma werkt op de
    arduino en de juiste info doorgeeft aan Excel

--- TSC230_6_commented/TSC230_6_commented.ino
+++ TSC230_6_commented/TSC230_6_commented.ino
@@ -3,7 +3,29 @@
 #include <Wire.h>
 
 //variabelen
+const unsigned long MEET_TIJD = 250000;   //de tijd die de sensor erover doet om een bepaalde kleur te meten, hoe langer hoe preciezer
+unsigned long Delay_tijd;
+const unsigned int AANTAL_METINGEN = 1;   //aantal metingen per gemiddelde
+#define S0     11
+#define S1     10
+#define S2     9
+#define S3     8
+#define OUT    2
+
 char Kleur[10] = "Rood";
+unsigned int   pulsen = 0;    // count the frequecy
+unsigned int   g_array[4];     // aantal pulsen per kleur
+unsigned long   g_SUM[4];     // store the RGB value no scaling SUM
+unsigned int   g_MAX[4];
+unsigned int   g_MIN[4];
+unsigned int   av_R;             //gemiddelde frequentie
+unsigned int   av_G;             //gemiddelde frequentie
+unsigned int   av_B;             //gemiddelde frequentie
+unsigned int   av_C;             //gemiddelde frequentie
+unsigned int   RGBC[4];         // store the RGB value
+unsigned int   g_flag = 0;     // filter of RGB queue
+float g_SF[3];        // save the RGB Scale factor
+ 
 
 
 // initialiseer de TSC230
@@ -25,29 +47,31 @@ void Sensor_init()
 // Selectreer de filter die wordt gebruikt || ROOD: LOW,LOW || BLAUW: LOW,HIGH || CLEAR: HIGH,LOW || GROEN: HIGH,HIGH
 void Verander_Filter(String Kleur)
 {
-  switch (Kleur)
-  {
-    case "rood":
+    if (Kleur == "rood")
+    {
       digitalWrite(S2, LOW); 
       digitalWrite(S3, LOW);
-      break;
-    case "blauw":
+    }
+    else if (Kleur == "blauw")
+    {
       digitalWrite(S2, LOW); 
       digitalWrite(S3, HIGH);
-      break;
-    case "clear":
+    }
+    else if (Kleur == "clear")
+    {
       digitalWrite(S2, HIGH); 
       digitalWrite(S3, LOW);
-      break;
-    case "groen":
+    }
+    else if (Kleur == "groen")
+    {
       digitalWrite(S2, HIGH); 
       digitalWrite(S3, HIGH);
-      break;
-    default:
+    }
+    else
+    {
       digitalWrite(S2, LOW); 
       digitalWrite(S3, LOW);
-      break;
-  }
+    }
 }
 
 
@@ -80,32 +104,98 @@ void TSC_Callback()
          Meet_kleur("rood");
          break;
     case 1:
-         g_array[0] = g_count;
+         g_array[0] = pulsen;
          Meet_kleur("groen");
          break;
     case 2:
-         g_array[1] = g_count;
+         g_array[1] = pulsen;
          Meet_kleur("blauw");
          break;
     case 3:
-         g_array[2] = g_count;
+         g_array[2] = pulsen;
          Meet_kleur("clear");
          break;
    default:
-         g_count = 0;
+         pulsen = 0;
          break;
   }
 }
- 
+
+
+void MAX_MIN_terugzetten ()
+{
+  g_SUM[0]=0;
+  g_SUM[1]=0;
+  g_SUM[2]=0;
+
+  g_MAX[0]=0;
+  g_MAX[1]=0;
+  g_MAX[2]=0;
+
+  g_MIN[0]=65535;
+  g_MIN[1]=65535;
+  g_MIN[2]=65535;
+}
+
+
+
+void Meting_uitvoeren (int aantal_metingen)
+{
+  MAX_MIN_terugzetten();
+  
+  for (int j=0; j<aantal_metingen; j++)
+  {
+   g_flag = 0;
+   for(int i=0; i<3; i++)
+   {
+    g_SUM[i] = g_SUM[i] + g_array[i];
+    if (g_MAX[i] < g_array[i])
+      g_MAX[i] = g_array[i];
+    if (g_MIN[i] > g_array[i])
+      g_MIN[i] = g_array[i];
+    RGBC[i] = int(g_array[i] * g_SF[i]);
+    /*Serial.print("->RGB =");
+    Serial.println(RGB[i]);*/
+   }
+   
+   delay(Delay_tijd);
+  }
+
+  av_R = g_SUM[0] / aantal_metingen;
+  av_G = g_SUM[1] / aantal_metingen;
+  av_B = g_SUM[2] / aantal_metingen;
+}
+
+
+void Gegevens_verz_excel ()
+{
+  Serial.print("DATA,TIME,");
+  Serial.print(av_R);
+  Serial.print(",");
+  Serial.print(av_G);
+  Serial.print(",");
+  Serial.println(av_B);  
+}
 
 
 
 void setup() {
-  // put your setup code here, to run once:
+  Sensor_init();
+  Serial.begin(9600);
+  Serial.println("CLEARDATA");
+  Serial.println("LABEL,TIMER,R,G,B");
+  Timer1.initialize();             // defaulte is 1s
+  Timer1.attachInterrupt(TSC_Callback);  
+  attachInterrupt(digitalPinToInterrupt(OUT), Pulsen_tellen, RISING);
 
+  Delay_tijd = MEET_TIJD * 4 /1000;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+  Meting_uitvoeren(AANTAL_METINGEN);
 
+  Gegevens_verz_excel();
+  
+ 
 }
